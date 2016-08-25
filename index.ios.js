5b999b776795f35d5b739947d07e305a6fbe5a34
scalkpdev <garvin.ling@kp.org>
2016-08-25 00:03:43 -0700

    Setup MapView and retrieve landmarks
    Setup React native maps and pull in landmark info from local API

--- index.ios.js
+++ index.ios.js
@@ -1,43 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
+import PerspectiveMap from './components/PerspectiveMap';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  AlertIOS,
+  requireNativeComponent
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
+// var MapView = require('react-native-maps');
+
 class PerspectiveApp extends Component {
+
+
+ constructor(props){
+
+    super(props);
+
+    const DEFAULT_LATITUDE  = 34.104581;
+    const DEFAULT_LONGITUDE = -118.145595;
+
+    this.state = {
+
+        user_latitude  : DEFAULT_LATITUDE,
+        user_longitude : DEFAULT_LONGITUDE,
+        landmarks      : []
+    }
+
+    this.getUserLocation();
+    this.getMarkersWithinArea();
+ }
+
+
+
+ getUserLocation() {
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+
+        this.setState({user_latitude : position.coords.latitude , user_longitude : position.coords.longitude});
+      },
+      (error) => {console.log(error)},
+    );
+
+ }
+ 
+
+
+ getMarkersWithinArea() {
+
+    fetch('http://localhost:3000/api/landmarks',{method:'GET'})
+      .then((response) => response.json())  
+        .then( (responseData) => 
+              {
+                console.log(responseData);
+                this.setState({landmarks:responseData});
+              }
+        )
+ }
+
+
   render() {
+
     return (
+
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+          <PerspectiveMap UserPosition={{lat:this.state.user_latitude,lng:this.state.user_longitude}} LandMarks={this.state.landmarks}/>
+         
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -47,6 +89,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
